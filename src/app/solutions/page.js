7f70d66e93653e227ae49e2d063ede573fdca7bb
Swiftdaops <tobepersonnalmail@gmail.com>
2026-02-15 02:58:36 +0100

    chore(solutions): use SectionDivider2 for section transitions

--- src/app/solutions/page.js
+++ src/app/solutions/page.js
@@ -3,7 +3,7 @@ import SearchEngineering from "../components/SearchEngineering";
 import RevenueAutomation from "../components/RevenueAutomation";
 import FinancialSystemsSection from "../components/FinancialSystemsSection";
 import ConversionScienceSection from "../components/ConversionScienceSection";
-import SectionDivider from "../components/SectionDivider";
+import SectionDivider from "../components/SectionDivider2";
 
 export default function SolutionsPage() {
 	return (
